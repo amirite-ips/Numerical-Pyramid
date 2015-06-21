1d1ed5fe1496971171988e503c1ea123d3c12db3
RomanCastellarin <RomanCastellarin@users.noreply.github.com>
2015-06-21 16:04:30 -0300

    classes skeletons added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
+
+class cell{
+    public:
+        void set( ... );
+    private:
+};
+
+class pyramid{
+    public:
+        bool canSolve();
+        string toString();
+    private:
+};
 
 int main(){
 
