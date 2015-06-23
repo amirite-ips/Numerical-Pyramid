27b15d6a7df63ff9fe2be8cdcc0c6ad6d9a932fe
RomanCastellarin <RomanCastellarin@users.noreply.github.com>
2015-06-22 23:24:23 -0300

    update 2

--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include <sstream>
+#include <queue>
 #define forn(a,b) for(int (a)=0; (a)<(int)(b); ++(a))
 using namespace std;
 
+typedef int intv;
+
+/** * * * * * * * * * * * * * *
+ *  ABUSING OOP TO ITS LIMITS *
+ * * * * * * * * * * * * * * **/
+
+/// CLASS CELL
 class cell{
     public:
-        cell() : unknown(true) {}
-        void set(int v);
-        int get();
+        cell() : unknown(true), value(0) {}
+        void set(intv v);
+        intv get();
+        bool isUnknown();
+
     private:
         bool unknown;
-        int value;
+        intv value;
 };
 
+/// CLASS PYRAMID
 class pyramid{
     public:
         pyramid(int n){
@@ -23,27 +34,58 @@ class pyramid{
         bool canSolve();
         bool solve();
         string toString();
-        cell& at(int x, int y){ return data[x][y]; }
+        cell& at(int x, int y);
+
     private:
         vector<vector<cell>> data;
 };
 
+/// MAIN FUNCTION
 int main(){
     pyramid *A;
+    string s;
 
     A = new pyramid(6);
 
-    A->at(1,3).set(4);
+    A->at(3,1).set(4);
+
+    s = A->toString();
+
+    cout<<s<<endl;
 
 
     return 0;
 }
 
-void cell::set(int v){
+/// CELL METHODS
+
+void cell::set(intv v){
     value = v;
     unknown = false;
 }
 
-int cell::get(){
+intv cell::get(){
     return value;
+}
+
+bool cell::isUnknown(){
+    return unknown;
+}
+
+/// PYRAMID METHODS
+
+string pyramid::toString(){
+    stringstream s;
+    for(auto& row : data){
+        forn(i, row.size()){
+            if( i ) s<<' ';
+            s<<row[i].get();
+        }
+        s<<endl;
+    }
+    return s.str();
+}
+
+cell& pyramid::at(int x, int y){
+    return data[x][y];
 }
