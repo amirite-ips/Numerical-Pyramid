833de6cd742b457e2d4a7e3292389d6a53262bc1
Juan <juaniisuar@gmail.com>
2015-06-22 22:31:08 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,3 +21,11 @@ int main(){
 
     return 0;
 }
+
+/** Como construir la pirámide:
+- Asignarle a cada casilla los numeros del 1 al 21 numerando desde arriba hacia abajo, 
+    siempre de izquierda a derecha
+- Asignarle un nivel 'y' a cada casilla, tal que la fila del 1 tenga nivel y=1,
+    la fila del 2 y el 3 con y=2, y así sucesivamente hasta la última con y=6
+- Para algana casilla con número 'x' en la pirámide, sus hijos son (x + y) y (x + y + 1)
+**/
