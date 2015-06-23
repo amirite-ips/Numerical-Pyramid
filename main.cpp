b596d60fc5cedb17d3bfe43518aa65f58721ee39
RomanCastellarin <RomanCastellarin@users.noreply.github.com>
2015-06-23 00:10:42 -0300

    added descriptions and minors tweaks
    we kind of should add some middle layer that handles the cells neighbors
    and stuff

--- main.cpp
+++ main.cpp
@@ -4,12 +4,11 @@
 #include <queue>
 #define forn(a,b) for(int (a)=0; (a)<(int)(b); ++(a))
 using namespace std;
-
 typedef int intv;
 
-/** * * * * * * * * * * * * * *
- *  ABUSING OOP TO ITS LIMITS *
- * * * * * * * * * * * * * * **/
+/** * * * * * * * * * * * * * * *
+ *  ABUSING OOP TO ITS LIMITS   *
+ * * * * * * * * * * * * * * * **/
 
 /// CLASS CELL
 class cell{
@@ -27,10 +26,7 @@ class cell{
 /// CLASS PYRAMID
 class pyramid{
     public:
-        pyramid(int n){
-            data.resize(n);
-            forn(i, n)  data[i].resize(i+1);
-        }
+        pyramid(int n){ data.resize(n); forn(i, n)  data[i].resize(i+1); }
         bool canSolve();
         bool solve();
         string toString();
@@ -42,50 +38,48 @@ class pyramid{
 
 /// MAIN FUNCTION
 int main(){
-    pyramid *A;
-    string s;
+    pyramid *A = new pyramid(6);
 
-    A = new pyramid(6);
+    A->at(3,1).set(43210);
 
-    A->at(3,1).set(4);
-
-    s = A->toString();
-
-    cout<<s<<endl;
-
+    cout<<A->toString()<<endl;
 
     return 0;
 }
 
 /// CELL METHODS
-
+/* set: assigns a new value to the cell and marks it as resolved */
 void cell::set(intv v){
     value = v;
     unknown = false;
 }
 
+/* get: returns the current value of the cell, it should be called if isUnknown
+        returned false */
 intv cell::get(){
     return value;
 }
 
+/* isUnknown: returns whether the cell's value has been resolved */
 bool cell::isUnknown(){
     return unknown;
 }
 
 /// PYRAMID METHODS
-
+/* toString: returns a string representation of the pyramid */
 string pyramid::toString(){
     stringstream s;
     for(auto& row : data){
         forn(i, row.size()){
             if( i ) s<<' ';
-            s<<row[i].get();
+            s.width(5), s<<row[i].get();
         }
         s<<endl;
     }
     return s.str();
 }
 
+/* at: returns a reference of the cell appointed by row x, column y */
 cell& pyramid::at(int x, int y){
     return data[x][y];
 }
