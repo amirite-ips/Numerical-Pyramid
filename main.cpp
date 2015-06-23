f17ff86be3641c3bd0577ae41eb06f59b0ba13a5
Juan <juaniisuar@gmail.com>
2015-06-23 20:11:25 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,10 +26,16 @@ int main(){
 }
 
 /** 
-    Como construir la pirámide:
-- Asignarle a cada casilla los numeros del 1 al 21 numerando desde arriba hacia abajo, 
-    siempre de izquierda a derecha
+    Como construir la pirámide (de base n):
+- Hacer n arrays de tamaño 1, 2, 3, ..., n
+- Para una celda en la posicion P[y][x], los hijos son P[y+1][x] y P[y+1][x+1]
+**/
+
+/** 
+    Otra forma:
+- Asignarle a cada casilla los numeros del 1 al f(n)=f(n-1)+n numerando desde arriba hacia abajo, 
+    siempre de izquierda a derecha (con f(1)=1)
 - Asignarle un nivel 'y' a cada casilla, tal que la fila del 1 tenga nivel y=1,
-    la fila del 2 y el 3 con y=2, y así sucesivamente hasta la última con y=6
-- Para algana casilla con número 'x' en la pirámide, sus hijos son (x + y) y (x + y + 1)
+    la segunda fila con y=2, y así sucesivamente hasta la última con y=n
+- Para algana casilla con número 'x' en la pirámide, sus hijos son (x+y) y (x+y+1)
 **/
